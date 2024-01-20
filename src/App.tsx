d3f44f7d7352622e6dfec1fb58d85ccbd2d00ebf
Carlos <carlos.msv@hotmail.com>
2024-01-20 03:07:23 -0300

    :sparkles:Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { Post } from "./Post"
+
 import { Header } from "./components/Header"
 import './globals.css'
+
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 function App() {
   return (
     <div>
       <Header/>
-      <Post 
-        author="Carlos Matheus" 
-        content="Lorem ipsum"
-      />
-      <Post 
-        author="Carlos Viana" 
-        content="Dolor sit"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author = "Carlos Matheus"
+            content = "Lorem ipsum"
+          />
+          <Post
+            author = "Carlos Viana"
+            content = "Lorem ipsum dolor"
+          />
+        </main>
+
+      </div>
     </div>
   )
 }
