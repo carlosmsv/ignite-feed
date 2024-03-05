f669d1c914ed31b237b6eec93b4433b915b85960
Carlos <carlos.msv@hotmail.com>
2024-03-05 01:56:53 -0300

    :techologist:indent code

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,45 +18,45 @@ export function Post({author, content, publishedAt}: Props) {
 		})
 
     return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src={author.avatarUrl}/>
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
-                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()} className={styles.date}>{publishedDateRelativeToNow}</time>
-            </header>
-            <div className={styles.content}>
-                {content.map( line => {
-									if (line.type === 'paragraph') {
-										return <p>{line.content}</p>
-									}
-									if (line.type === 'link') {
-										return <p><a href="#">{line.content}</a></p>
-									}
-								})}
-            </div>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+              <Avatar src={author.avatarUrl}/>
+              <div className={styles.authorInfo}>
+                  <strong>{author.name}</strong>
+                  <span>{author.role}</span>
+              </div>
+          </div>
+          <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()} className={styles.date}>{publishedDateRelativeToNow}</time>
+        </header>
+        <div className={styles.content}>
+          {content.map( line => {
+            if (line.type === 'paragraph') {
+              return <p>{line.content}</p>
+            }
+            if (line.type === 'link') {
+              return <p><a href="#">{line.content}</a></p>
+            }
+        })}
+        </div>
 
-            <form className= {styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+        <form className= {styles.commentForm}>
+          <strong>Deixe seu feedback</strong>
 
-                <textarea
-                    placeholder="Deixe um comentário"
-                />
-                <footer>
-                    <button type="submit">Publicar</button>
-                </footer>
-            </form>
+          <textarea
+            placeholder="Deixe um comentário"
+          />
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
 
-            <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
-            </div>
-        </article>
+        <div className={styles.commentList}>
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
+      </article>
     )
 }
   
